7e2495987b9bf37eca84ebb6676df937f392af0d
jonewan <jonewan@yeah.net>
2019-08-21 10:11:46 +0800

    test_revword

---
+++ test_revword/test_revword.cc
@@ -0,0 +1,22 @@
+/*================================================================
+*   Copyright (C) 2019 Sangfor Ltd. All rights reserved.
+*   
+*   fileName：test_revword.cpp
+*   author：Jonewan
+*   date：2019-08-14
+*   discription：
+*
+================================================================*/
+
+
+#include <iostream>
+
+using namespace std;
+
+int main(void)
+{
+
+	cout << "hello" << endl;
+
+	return 0;
+}
